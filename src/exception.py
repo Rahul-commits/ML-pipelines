bf8bc9435a7b1fbe0c09c4a15b023b5297f71a41
Rahul-commits <rahul.narramaneni>
2023-06-29 02:32:57 -0400

    Data Transformation Added

--- src/exception.py
+++ src/exception.py
@@ -1,15 +1,15 @@
 import sys
 
 
-def error_message_details(error, error_detail:sys):
+def error_message_details(error, error_detail:sys): # type: ignore
     _,_,exec_tb=error_detail.exc_info()
-    file_name=exec_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line mumber [{1}] error message[{2}]".format(file_name,exec_tb.tb_lineno,str(error))
+    file_name=exec_tb.tb_frame.f_code.co_filename # type: ignore
+    error_message="Error occured in python script name [{0}] line mumber [{1}] error message[{2}]".format(file_name,exec_tb.tb_lineno,str(error)) # type: ignore
 
     return error_message
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_detail:sys): # type: ignore
         super().__init__(error_message)
         self.error_message=error_message_details(error_message,error_detail=error_detail)
 
